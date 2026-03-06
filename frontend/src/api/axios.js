0361671fabf3e5836d43c7b88096400202285037
SurajKumar798 <128129426+SurajKumar798@users.noreply.github.com>
2026-03-06 18:28:28 +0530

    Update axios.js

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: 'https://ecommerce-backend-ln2c.onrender.com',
+    baseURL: 'https://ecommerce-backend-t4vy.onrender.com/api',
 })
 
 export default api;
